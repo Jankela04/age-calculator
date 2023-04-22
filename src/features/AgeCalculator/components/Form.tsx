186c7dac73de2d5966ad97c42fb430807f2d6db7
Jankela04 <janezjovanovic04@gmail.com>
2023-04-22 16:19:26 +0200

    change from mui form to formik

--- src/features/AgeCalculator/components/Form.tsx
+++ src/features/AgeCalculator/components/Form.tsx
@@ -1,44 +1,83 @@
 import { Input } from "@/components/Input";
 import { Box, Button } from "@mui/material";
 import SubmitIcon from "@/assets/icon-arrow.svg";
-import { z } from "zod";
+import { Formik, Form as FormikForm, Field } from "formik";
+import { formSchema, TForm } from "../formSchema";
 
-const formSchema = z.object({
-    day: z.number({ required_error: "Required" }).min(1).max(31),
-    month: z.number({ required_error: "Required" }).min(1).max(12),
-    year: z
-        .number({ required_error: "Required" })
-        .positive()
-        .max(new Date().getFullYear()),
-});
+const initialValues = {
+    day: "",
+    month: "",
+    year: "",
+};
+
+const handleSubmit = (data: TForm) => {
+    // todo validate if date is valid
+    console.log(data);
+};
 
 export const Form = () => {
     return (
-        <Box component="form" autoComplete="off" sx={{ width: "100%" }}>
-            <Box sx={{ display: "flex", gap: 2, justifyContent: "center" }}>
-                <Input inputlabel="day" placeholder="DD" />
-                <Input inputlabel="month" placeholder="MM" />
-                <Input inputlabel="year" placeholder="YYYY" />
-            </Box>
-            <Box
-                sx={{
-                    py: 4,
-                    display: "flex",
-                    justifyContent: { xs: "center", sm: "flex-end" },
-                }}
-            >
-                <Button
-                    type="submit"
-                    variant="contained"
-                    sx={{
-                        padding: 2,
-                        borderRadius: "50%",
-                        ":hover": { bgcolor: "black" },
-                    }}
-                >
-                    <img src={SubmitIcon} alt="submit icon" />
-                </Button>
-            </Box>
-        </Box>
+        <Formik
+            validationSchema={formSchema}
+            //@ts-ignore
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+        >
+            {({ errors, touched }) => (
+                <FormikForm style={{ width: "100%" }} autoComplete="off">
+                    <Box
+                        sx={{
+                            display: "flex",
+                            gap: 2,
+                            justifyContent: "center",
+                        }}
+                    >
+                        <Field
+                            as={Input}
+                            name="day"
+                            placeholder="DD"
+                            inputlabel="day"
+                            error={touched.day && Boolean(errors.day)}
+                            helperText={touched.day && errors.day}
+                        />
+                        <Field
+                            as={Input}
+                            name="month"
+                            placeholder="MM"
+                            inputlabel="month"
+                            error={touched.month && Boolean(errors.month)}
+                            helperText={touched.month && errors.month}
+                        />
+                        <Field
+                            as={Input}
+                            name="year"
+                            placeholder="YYYY"
+                            inputlabel="year"
+                            error={touched.year && Boolean(errors.year)}
+                            helperText={touched.year && errors.year}
+                        />
+                    </Box>
+                    <Box
+                        sx={{
+                            py: 4,
+                            display: "flex",
+                            justifyContent: { xs: "center", sm: "flex-end" },
+                        }}
+                    >
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            sx={{
+                                padding: 2,
+                                borderRadius: "50%",
+                                ":hover": { bgcolor: "black" },
+                            }}
+                        >
+                            <img src={SubmitIcon} alt="submit icon" />
+                        </Button>
+                    </Box>
+                </FormikForm>
+            )}
+        </Formik>
     );
 };
