8b3c5b9c37070f129952e066b3f9317f500223d8
Jankela04 <janezjovanovic04@gmail.com>
2023-04-30 15:18:35 +0200

    fix schema validation

--- src/features/AgeCalculator/formSchema.ts
+++ src/features/AgeCalculator/formSchema.ts
@@ -8,7 +8,20 @@ export const formSchema = Yup.object().shape({
             "is-valid-day",
             "Must be valid day",
             (value) => value >= 1 && value <= 31
-        ),
+        )
+        .test("is-valid-day", "Must be valid date", function (value) {
+            const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+            if (
+                (this.parent.year % 4 == 0 && this.parent.year % 100 != 0) ||
+                this.parent.year % 400 == 0
+            ) {
+                months[1] = 29;
+            }
+            if (value > months[this.parent.month - 1]) {
+                return false;
+            }
+            return true;
+        }),
     month: Yup.number()
         .required("Required")
         .typeError("Must be valid month")
@@ -21,7 +34,7 @@ export const formSchema = Yup.object().shape({
         .required("Required")
         .typeError("Must be valid year")
         .positive("Must be valid year")
-        .max(new Date().getFullYear(), "Year cannot be in the future"),
+        .max(new Date().getFullYear(), "Must be in the past"),
 });
 
 export type TForm = Yup.InferType<typeof formSchema>;
